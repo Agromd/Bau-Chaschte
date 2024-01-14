8f1617179568b0373a80bc3386a531d80d4c5de3
Vladimir Fedin <vlmaxfd@gmail.com>
2024-01-14 22:12:26 +0100

    new images lot of code changes

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ const themeSwitcher = document.getElementById('theme-switch');
 themeSwitcher.addEventListener('change', () => {
     document.documentElement.classList.toggle('dark-mode-variables');
 });
+
+const slider = document.querySelector('.galary-slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentSlide = 0;
+
+nextBtn.addEventListener('click', () => {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  updateSlider();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  updateSlider();
+});
+
+function updateSlider() {
+  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
     // You might need to apply different styles to specific elements based on the theme
     // You can use document.querySelectorAll or getElement(s)ByClassName/Id
     // For example:
