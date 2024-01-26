35137c2d4f08b2eba6869fb9bb38ad37cd947a59
Vladimir Fedin <vlmaxfd@gmail.com>
2024-01-26 15:18:16 +0100

    Loader added

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-const themeSwitcher = document.getElementById('theme-switch');
 
-themeSwitcher.addEventListener('change', () => {
-    document.documentElement.classList.toggle('dark-mode-variables');
-});
+// theme swither tool
+// const themeSwitcher = document.getElementById('theme-switch');
 
-const slider = document.querySelector('.galary-slider');
-const slides = document.querySelectorAll('.slide');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-let currentSlide = 0;
-
-nextBtn.addEventListener('click', () => {
-  currentSlide++;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-  updateSlider();
-});
-
-prevBtn.addEventListener('click', () => {
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-  updateSlider();
-});
-
-function updateSlider() {
-  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
+// themeSwitcher.addEventListener('change', () => {
+//     document.documentElement.classList.toggle('dark-mode-variables');
+// });
     // You might need to apply different styles to specific elements based on the theme
     // You can use document.querySelectorAll or getElement(s)ByClassName/Id
     // For example:
@@ -47,3 +23,14 @@ function toggleSidebar() {
   const content = document.querySelector('.content');
   content.style.marginLeft = content.style.marginLeft === '250px' ? '0' : '250px';
 }
+
+// loader
+
+window.addEventListener("load", () =>{
+  const loader = document.querySelector(".loader")
+
+  loader.classList.add("loader--hidden");
+  loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+  });
+});
