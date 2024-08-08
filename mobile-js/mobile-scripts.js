42b99abc5b5068cb3259e08b3d2cfa4f62308ef4
Vladimir Fedin <vlmaxfd@gmail.com>
2024-08-08 16:26:37 +0300

    mobile version finished!!!

--- mobile-js/mobile-scripts.js
+++ mobile-js/mobile-scripts.js
@@ -1,14 +1,66 @@
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("top_btn").style.display = "block";
+  } else {
+    document.getElementById("top_btn").style.display = "none";
   }
-  
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("top_btn").style.display = "block";
-    } else {
-      document.getElementById("top_btn").style.display = "none";
-    }
-  }
+}
+
+//ham menu js start
+
+const hamMenu = document.querySelector('.header_hamb-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
+
+// Close menu when any link is clicked
+const menuLinks = offScreenMenu.querySelectorAll('a');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    hamMenu.classList.remove('active');
+    offScreenMenu.classList.remove('active');
+  });
+});
+
+//ham menu js end
+
+//swiper js start
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: 'slider',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.s-button-next',
+    prevEl: '.s-button-prev',
+  },
+
+  // autoplay: {
+  //   delay: 3000,
+  // },
+
+});
+
+
+//swiper js end
+
+
